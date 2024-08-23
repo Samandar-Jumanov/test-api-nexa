8887053630be2fe74cec7a080c7df17d48df3ea9
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-08-23 10:32:40 +0300

    Updating data

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.get("/sales", (req, res) => {
+    res.json([
+        {
+            name: "Product One",
+            data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 45],
+        },
+        {
+            name: "Product Two",
+            data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39, 51],
+        }
+    ]);
+});
 
-
-app.get("/sales" , (  req , res ) => {
-        res.json({
-               name : 'Sales',
-               data : [2, 33, 11, 22]
-        })
-})
-
-
-app.listen(3001 , ( ) => {
-         console.log("Server started on port 3001")
-})
+app.listen(3001, () => {
+    console.log("Server started on port 3001");
+});
